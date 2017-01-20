98a723e4f3b8893721bc7c7d3e498e750361da15
David Mandel <mandeldl@gmail.com>
2017-01-20 18:02:04 -0500

    add models

---
+++ models.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+// Define AnswerSchema before QuestionSchema since the latter references the former:
+var AnswerSchema = new Schema({
+	text: String,
+	createdAt: {type: Date, default: Date.now},
+	updatedAt: {type: Date, default: Date.now},
+	votes: {type: Number, default: 0}
+});
+
+var QuestionSchema = new Schema({
+	text: String,
+	createdAt: {type: Date, default: Date.now},  //set to current date and time
+	answers: [AnswerSchema]  //indicates to Mongo that the answers will be an array of Answerschemas
+});
+
+QuestionSchema.pre('save', function (next) {
+	//sort answers array before calling next
+	this.answers.sort(function(a,b){ return a.votes-b.votes; });
+	next();
+})
+
+var Question = mongoose.model("Question", QuestionSchema);
+
+module.exports.Question = Question;
