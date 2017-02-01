bd432c92596a07b13e005ea2d85eb05a16d22ffc
David Mandel <mandeldl@gmail.com>
2017-02-01 17:18:40 -0500

    add CORS headers

--- app.js
+++ app.js
@@ -27,6 +27,17 @@ db.once('open', function () {
 	console.log('db connection successful');
 });
 
+// Custom headers to allow CORS:
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', *);
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	//
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE');
+		return res.status(200).json({});
+	}
+	next();
+});
 
 app.use('/questions', routes);
 
